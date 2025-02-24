452c8b7471ba9f751bcec09c7890dfdf76f06f57
PulkitT01 <pulkitthukral001@gmail.com>
2025-02-25 00:53:57 +0100

    Add results for baseline models run for 10 iterations

--- main_propensity_dropout.py
+++ main_propensity_dropout.py
@@ -31,6 +31,6 @@ from DCN.Model25_10_25 import Model_25_1_25
 
 if __name__ == '__main__':
     print("Using original data")
-    Experiments().run_all_experiments(iterations=1, running_mode="original_data")
+    Experiments().run_all_experiments(iterations=10, running_mode="original_data")
     # Model_25_1_25().run_all_expeiments()
     # Graphs().draw_scatter_plots()
