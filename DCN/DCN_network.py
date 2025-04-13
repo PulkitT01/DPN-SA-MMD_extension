d122d9c7cfb7287490253394c96a5094d271dfb7
Pulkit Thukral <85827394+PulkitT01@users.noreply.github.com>
2025-04-13 13:01:41 +0200

    Update DCN_network.py to accomodate IHDP data

--- DCN/DCN_network.py
+++ DCN/DCN_network.py
@@ -1,27 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2020 Shantanu Ghosh
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from collections import OrderedDict
 
 import torch
@@ -199,7 +175,11 @@ class DCN_network:
         T_list = []
 
         for batch in treated_data_loader:
-            covariates_X, ps_score, y_f, t, e = batch
+            if eval_parameters.get("has_e", True):  # Default: Jobs data
+                covariates_X, ps_score, y_f, t, e = batch
+            else:  # For IHDP
+                covariates_X, ps_score, y_f, t = batch
+                e = torch.zeros_like(t)  # or dummy value
             covariates_X = covariates_X.to(device)
             ps_score = ps_score.squeeze().to(device)
             treatment_pred = network(covariates_X, ps_score)
@@ -224,7 +204,11 @@ class DCN_network:
             predicted_ITE_list.append(predicted_ITE.item())
 
         for batch in control_data_loader:
-            covariates_X, ps_score, y_f, t, e = batch
+            if eval_parameters.get("has_e", True):  # Default: Jobs data
+                covariates_X, ps_score, y_f, t, e = batch
+            else:  # For IHDP
+                covariates_X, ps_score, y_f, t = batch
+                e = torch.zeros_like(t)  # or dummy value
             covariates_X = covariates_X.to(device)
             ps_score = ps_score.squeeze().to(device)
 
